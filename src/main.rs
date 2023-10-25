1598e0c1d90c97d8f193d65dbc7c3a16142d86be
Дмитрий Филин <dafilin@sberbank.ru>
2023-10-25 16:52:28 +0300

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::io::Write;
+use std::io::{Read, Write};
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use regex::Regex;
 
 const HTTP_OK: &str = "HTTP/1.1 200 OK\r\n\r\n";
+const HTTP_NOT_FOUND: &str = "HTTP/1.1 404 Not Found\r\n\r\n";
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,7 +18,20 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                _stream.write_all(HTTP_OK.as_bytes()).unwrap();
+                let mut buf: Vec<u8> = vec![];
+                let req = _stream.read_to_end(&mut buf).unwrap();
+                for (i, line) in req.to_string().lines().enumerate() {
+                    if i == 0 {
+                        let re = Regex::new(r"GET (.*) HTTP/1.1").unwrap();
+                        let cap = re.captures(line).unwrap();
+                        let path = cap.get(1).unwrap().as_str();
+                        if path == "/" {
+                            _stream.write_all(HTTP_OK.as_bytes()).unwrap();
+                        } else {
+                            _stream.write_all(HTTP_NOT_FOUND.as_bytes()).unwrap();
+                        }
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
