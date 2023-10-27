e1e90669a1a3e6a7d222f917802979dfa6d680d1
Дмитрий Филин <dafilin@sberbank.ru>
2023-10-27 15:19:18 +0300

    pass 8th stage

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,10 @@ async fn handle_post_request(path: &str, reader: &mut BufReader<OwnedReadHalf>, 
                 if line.starts_with("Content-Length") {
                     content_length = parse_http_line(line.as_str(), r"Content-Length: (.*)\n").unwrap().parse().unwrap();
                     println!("Expected content len: {}", content_length);
+                    line.clear();
                     reader.read_line(&mut line).await.unwrap();
+                    println!("{}", line);
+                    line.clear();
                     reader.read_line(&mut line).await.unwrap();
                     println!("{}", line);
                     break;
